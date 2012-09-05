78262fac30d3413b98637b45bd49c9d62839844d
Thomas Rieder <tho.rieder@aon.at>
2012-09-05 21:57:07 +0200

    y-axis pid tuned

--- quadcopter_mkii_arduino/quadcopter_mkii_arduino.ino
+++ quadcopter_mkii_arduino/quadcopter_mkii_arduino.ino
@@ -15,6 +15,7 @@ boolean debug_serial = false;
 
 boolean use_imu = true;
 boolean use_motors = true;
+boolean en_motors = false;
 
 int i, j, k;
 
@@ -23,17 +24,17 @@ float angles[3];
 double xAngle, yAngle;
 
 int xOffsetIMU = -4;
-int yOffsetIMU = 0;
+int yOffsetIMU = -3;
 
 Servo esc_x1;
 Servo esc_x2;
 Servo esc_y1;
 Servo esc_y2;
 
-int esc_x1_pin = 3; // verified
-int esc_x2_pin = 11; // verified
-int esc_y1_pin = 10; // verified
-int esc_y2_pin = 9; // verified
+int esc_x1_pin = 3;    // verified
+int esc_x2_pin = 11;   // verified
+int esc_y1_pin = 10;   // verified
+int esc_y2_pin = 9;    // verified
 
 double xSpeed = 1000;
 double ySpeed = 1000;
@@ -41,12 +42,13 @@ double ySpeed = 1000;
 double xPIDSpeed;
 double yPIDSpeed;
 
-double targetAngle = 180.0;
+double targetAngleX = 180.0;
+double targetAngleY = 180.0;
 
-PID xPID(&xAngle, &xPIDSpeed, &targetAngle, 0.4, 0.05, 0.2, DIRECT); // .7, .1, .2
+PID xPID(&xAngle, &xPIDSpeed, &targetAngleX, 0.4, 0.05, 0.2, DIRECT); // .7, .1, .2
 //PID xPID(&xAngle, &xPIDSpeed, &targetAngle, 0.25, 0.05, 0.1, DIRECT); // .7, .1, .2
 //PID xPID(&xAngle, &xPIDSpeed, &targetAngle, 0.55, 0.1, 0.3, DIRECT); // .7, .1, .2
-PID yPID(&yAngle, &yPIDSpeed, &targetAngle, 0.4, 0.05, 0.2, DIRECT);
+PID yPID(&yAngle, &yPIDSpeed, &targetAngleY, 1.0, 0.05, 0.2, DIRECT);
 
 
 // Remote Control Stuff
@@ -54,11 +56,13 @@ PID yPID(&yAngle, &yPIDSpeed, &targetAngle, 0.4, 0.05, 0.2, DIRECT);
 // Contains the issued command
 char command[6];
 
-// Contains the commands parameter
+// Contains the command-parameter
 char value[6];
 
 char tempChar;
 int number;
+
+boolean flush_needed = false;
 
 
 
@@ -83,8 +87,8 @@ void setup() {
     esc_y2.attach(esc_y2_pin);
     
     // set speed to zero
-    setSpeedX(1000);
-    setSpeedY(1000);
+    setSpeedX(xSpeed);
+    setSpeedY(ySpeed);
     
     xPID.SetOutputLimits(-20, 20);
     xPID.SetMode(AUTOMATIC);
@@ -99,6 +103,8 @@ void setup() {
     // Motor debug variable
     j = 0;
   }
+  
+  Serial.print("Setup complete");
 }
 
 
@@ -110,15 +116,15 @@ void loop() {
     yAngle = angles[0] + 180 + yOffsetIMU;
   }
   
-  if(use_motors && xSpeed != 999 && ySpeed != 999) {
+  if(use_motors && en_motors) {
     xPID.Compute();
     yPID.Compute();
     
     setSpeedESC(esc_x1, xSpeed - xPIDSpeed/2);
     setSpeedESC(esc_x2, xSpeed + xPIDSpeed/2);
     
-    setSpeedESC(esc_y1, ySpeed - yPIDSpeed/2);
-    setSpeedESC(esc_y2, ySpeed + yPIDSpeed/2);
+    setSpeedESC(esc_y1, ySpeed + yPIDSpeed/2);
+    setSpeedESC(esc_y2, ySpeed - yPIDSpeed/2);
     
     
     if(xSpeed < 1250) {
@@ -132,35 +138,55 @@ void loop() {
     }
     
     if(ySpeed < 1250) {
-      yPID.SetOutputLimits(-20, 20);
+      yPID.SetOutputLimits(-50, 50);
     } else if(ySpeed >= 1250 && ySpeed < 1350) {
-      yPID.SetOutputLimits(-12, 12);
+      yPID.SetOutputLimits(-30, 30);
     } else if(ySpeed >= 1350 && ySpeed < 1400) {
-      yPID.SetOutputLimits(-8, 8);
+      yPID.SetOutputLimits(-15, 15);
     } else if(ySpeed >= 1400 && ySpeed < 2000) {
-      yPID.SetOutputLimits(-5, 5);
+      yPID.SetOutputLimits(-10, 10);
     }
   }
   
- 
-  k = 0;
-  while(Serial.available() > 0 && k < 5) {
-    tempChar = Serial.read();
-    command[k] = tempChar;
-    k++;
-    command[k] = '\0';
+  // Read the serial buffer
+  if(Serial.available() >= 10) {
+    // 10 bytes available
+    
+    k = 0;
+    while(k < 5) {
+      tempChar = Serial.read(); 
+      command[k] = tempChar;
+      k++;
+      command[k] = '\0';
+      if(tempChar == '-') {
+        flush_needed = true;
+      }
+    }
+    
+    k = 0;
+    while(k < 5) {
+      tempChar = Serial.read();
+      value[k] = tempChar;
+      k++;
+      value[k] = '\0';
+    }
+    
+    // convert the second parameter to int
+    number = atoi(&value[0]);
   }
   
-  k = 0;
-  while(Serial.available() > 0 && k < 5) {
-    tempChar = Serial.read();
-    value[k] = tempChar;
-    k++;
-    value[k] = '\0';
+  if(flush_needed) {
+    Serial.print("flushing_ser_cache;");
+    
+    // Clear Buffer
+    while(Serial.available()>0) {
+      Serial.read();
+    }
+    
+    flush_needed = false;
+    number = 0;
   }
   
-  
-  number = atoi(&value[0]);
   
   if(number != 0) {
     // X-Axis
@@ -193,6 +219,23 @@ void loop() {
       Serial.print(xSpeed);
       Serial.print(";");
     } 
+    
+    // Set Angles
+    else if(strcmp(command, "setXA") == 0) {
+      Serial.print("setXA.Ack;");
+      if(number >= 140 && number <= 220) {
+        targetAngleX = number;
+      }
+      Serial.print(targetAngleX);
+      Serial.print(";");
+    } else if(strcmp(command, "setYA") == 0) {
+      Serial.print("setYA.Ack;");
+      if(number >= 140 && number <= 220) {
+        targetAngleY = number;
+      }
+      Serial.print(targetAngleY);
+      Serial.print(";");
+    }
     
     // Individual Servos
     else if(strcmp(command, "setX1") == 0) {
@@ -233,14 +276,15 @@ void loop() {
       }
       Serial.print(yOffsetIMU);
       Serial.print(";");
-    } else if(strcmp(command, "enMTR") == 0) {
+    } 
+    
+    // Enable Motors
+    else if(strcmp(command, "enMTR") == 0) {
       Serial.print("enMTR.Ack;");
       if(number == 1) {
-        xSpeed = 999;
-        ySpeed = 999;
+        en_motors = false;
       } else if (number == 2) {
-        xSpeed = 1000;
-        ySpeed = 1000;
+        en_motors = true;
       }
     } 
     
@@ -273,6 +317,7 @@ void loop() {
     
   command[0] = '\0';
   value[0] = '\0';
+  number = 0;
 }
 
 void debug() {
@@ -280,11 +325,11 @@ void debug() {
     // Anti-Spam
     if(i == 100) {
       imu.print(true, true);
-      Serial.print("X: ");
+      Serial.print("X:");
       Serial.print(xAngle);
-      Serial.print("\t Y:");
+      Serial.print(";Y:");
       Serial.print(yAngle);
-      Serial.print('\n');
+      Serial.print(';');
       i = 0;  
     }
     i++;
@@ -293,22 +338,22 @@ void debug() {
   if(debug_motors) {
     if(j == 100) {
       j = 0;
-      Serial.print("x-PID: ");
+      Serial.print("x-PID:");
       Serial.print(xPIDSpeed);
-      Serial.print("\t y-PID: ");
+      Serial.print(";y-PID:");
       Serial.print(yPIDSpeed);
-      Serial.print('\n');
+      Serial.print(';');
     }
     j++;
   }
   
   if(debug_serial) {
     if(strcmp(command, "") != 0) {
-      Serial.print("\nCommand: ");
+      Serial.print("Command:");
       Serial.print(command);
-      Serial.print(" Value: ");
+      Serial.print(";Value:");
       Serial.print(value);
-      Serial.print("\n");
+      Serial.print(';');
     }
   }
 }
